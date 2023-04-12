5a09827cafdabbcd5a4e8925a247d066e13dfae8
Nihat <abdullazadenihat@gmail.com>
2023-04-12 19:54:12 +0400

    added objects

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const gameDiv = document.querySelector('.game')
-const random = (max, min = 0) => min + Math.floor(Math.random()* max  )
-
-const game = {
-  width : 400 ,
-  height : 400,
+const random = (max, min = 0) => min + Math.floor(Math.random() * max)
+const ball = document.querySelector('.ball')
+const { log } = console
+const Game = {
+  width: 400,
+  height: 400,
 
 }
 
-const ball = {
+const Ball = {
   width: 40,
   height: 40,
-
+  maxSpeed: 3,
+  x: 100,
+  y: 100,
+  movingX : 1,
+  movingY : 1,
 }
 
 const me = {
@@ -20,8 +25,8 @@ const me = {
 }
 
 function createObstacles(count = 10) {
-  let height = random(10,10);
-  let width = random(10 , 30);
+  let height = random(10, 10);
+  let width = random(10, 30);
   let offsetHeight = 0;
   for (let i = 0; i < count; i++) {
     let div = document.createElement('div');
@@ -31,12 +36,30 @@ function createObstacles(count = 10) {
     div.style.position = "absolute"
 
 
-    div.style.left = i*width + 15 +  "px";
-    div.style.top =  + "px";
+    div.style.left = i * width + 15 + "px";
+    div.style.top = offsetHeight + "px";
     div.style.backgroundColor = "#aa00aa"
     gameDiv.append(div)
   }
 }
 
+function moveBall() {
+  if (Ball.x + Ball.left > Game.width) {
+    Ball.x += 2;
+    Ball.y += 2;
+  }
+  ball.style.left = Ball.x + Ball.movingX +  "px"
+  ball.style.top = Ball.y + Ball.movingY +  "px";
 
-createObstacles()
+
+}
+
+
+function startGame() {
+  createObstacles()
+  setInterval(moveBall, 1000 / 60)
+
+}
+
+startGame()
+
--- dist/index.7c0ccee6.js
+++ dist/index.7c0ccee6.js
@@ -1,12 +1,19 @@
 const gameDiv = document.querySelector(".game");
 const random = (max, min = 0)=>min + Math.floor(Math.random() * max);
-const game = {
+const ball = document.querySelector(".ball");
+const { log  } = console;
+const Game = {
     width: 400,
     height: 400
 };
-const ball = {
+const Ball = {
     width: 40,
-    height: 40
+    height: 40,
+    maxSpeed: 3,
+    x: 100,
+    y: 100,
+    movingX: 1,
+    movingY: 1
 };
 const me = {
     width: 50,
@@ -23,11 +30,23 @@ function createObstacles(count = 10) {
         div.style.height = height + "px";
         div.style.position = "absolute";
         div.style.left = i * width + 15 + "px";
-        div.style.top = NaN;
+        div.style.top = offsetHeight + "px";
         div.style.backgroundColor = "#aa00aa";
         gameDiv.append(div);
     }
 }
-createObstacles();
+function moveBall() {
+    if (Ball.x + Ball.left > Game.width) {
+        Ball.x += 2;
+        Ball.y += 2;
+    }
+    ball.style.left = Ball.x + Ball.movingX + "px";
+    ball.style.top = Ball.y + Ball.movingY + "px";
+}
+function startGame() {
+    createObstacles();
+    setInterval(moveBall, 1000 / 60);
+}
+startGame();
 
 //# sourceMappingURL=index.7c0ccee6.js.map
